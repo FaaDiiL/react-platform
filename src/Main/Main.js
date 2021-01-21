88ca5bdd09903d383109c72aa012755b49eb885e
eyadal <31968847+eyadal@users.noreply.github.com>
2021-01-21 16:08:30 +0100

    Main Pull

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+import React, { useState } from 'react'
+
+import AddListItem from '../AddListItem/AddListItem'
+import List from '../List/List'
+
+function Main() {
+     
+     const [inputVal,setInputVal] = useState('Default Todo')
+     const [liVal,setLiVal] = useState('Default Todo')
+
+     const handleChange = (e) => {
+          setInputVal(e.target.value)
+     }
+
+     const handleOnSubmit = () => {
+          setLiVal(inputVal)
+     }
+
+     return (
+          <>
+               <AddListItem handleOnChange={handleChange} handleSubmit={handleOnSubmit} />
+               <List mainSetLi={liVal} />
+          </>
+     )
+}
+
+export default Main
+=======
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
@@ -20,3 +49,4 @@ function Main() {
 }
 
 export default Main
+>>>>>>> 7069325c751bdb7d133543fdddcb4484e425c886
