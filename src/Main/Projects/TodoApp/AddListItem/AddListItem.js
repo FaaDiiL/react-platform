966691c2d01606630ba2f525c2d6beac43a1631d
kayzersozee <63726574+kayzersozee@users.noreply.github.com>
2021-01-23 11:09:25 +0100

    merging oneMore

--- src/Main/Projects/TodoApp/AddListItem/AddListItem.js
+++ src/Main/Projects/TodoApp/AddListItem/AddListItem.js
@@ -6,11 +6,7 @@ function AddListItem(props) {
     return (
         <form onSubmit={props.handleSubmit}>
             <input onChange={props.handleOnChange}  />  
-<<<<<<< HEAD
             <button >Add Todo</button>
-=======
-            <button>Add Todo</button>
->>>>>>> 93081f9d94544fc4629e5aafaf249896dd518049
         </form>
     )
 }
