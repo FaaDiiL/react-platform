35ccb37a08a051fe9ce273078397dacca41798b3
Fadil Al <fadil.alfarhan@gmail.com>
2021-01-24 14:07:22 +0100

    Adding style from styled-components in Header.js and Navigation.js (Finnish Goup Lab)

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,19 @@
+import styled from 'styled-components'
+
 import Navigation from './Navigation/Navigation'
+
+const StyledHeader = styled.header`
+color: #fff;
+background-color: #370617;
+width: 100vw;
+`
 
 function Header(props) {
      return (
-          <>
+          <StyledHeader>
                <h1>{props.title}</h1>
                <Navigation />
-          </> 
+          </StyledHeader> 
      )
 }
 
--- src/Header/Navigation/Navigation.js
+++ src/Header/Navigation/Navigation.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import styled from 'styled-components'
+
+const StyledUl = styled.ul`
+list-style: none;
+`
+
+const StyledLi = styled.li`
+background-color:  #370637;
+line-height: 2.5rem;
+margin: 5px 0px;
+`
+
 function Navigation() {
     return (
-        <ul>
-            <li><Link to='/'>Home</Link></li>  
-            <li><Link to='/about'>About</Link></li>  
-            <li><Link to='/projects'>Projects</Link></li>  
-            <li><Link to='/contact'>Contact</Link></li>
-        </ul>
+        <StyledUl>
+            <StyledLi><Link to='/'>Home</Link></StyledLi>  
+            <StyledLi><Link to='/about'>About</Link></StyledLi>  
+            <StyledLi><Link to='/projects'>Projects</Link></StyledLi>  
+            <StyledLi><Link to='/contact'>Contact</Link></StyledLi>
+        </StyledUl>
     )
 }
 
