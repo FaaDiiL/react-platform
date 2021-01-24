4a1cf76ce007885a6c52f35cf5e7ebb9cbfd8a25
Fadil Al <fadil.alfarhan@gmail.com>
2021-01-24 12:39:18 +0100

    Created a Todo.js withe <li>'s in it, renderit in ViewTodos.js

---
+++ src/Main/Projects/TodoApp/ViewTodos/Todo/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo() {
+     return (
+          <>
+               <li>Todo</li>
+               <li>Todo</li>
+               <li>Todo</li>
+               <li>Todo</li>
+          </>
+     )
+}
+
+export default Todo
--- src/Main/Projects/TodoApp/ViewTodos/ViewTodos.js
+++ src/Main/Projects/TodoApp/ViewTodos/ViewTodos.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import Todo from './Todo/Todo'
+
 function ViewTodos() {
      return (
           <>
-             <h4>This is your list of todos!</h4>  
+             <h4>This is your list of todos!</h4>
+             <ul>
+               <Todo /> { /* Prints a list of Li's */}
+             </ul>
           </>
      )
 }
