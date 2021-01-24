1d73be649c0477763cc070d0561e2ac3da4fa660
Fadil Al <fadil.alfarhan@gmail.com>
2021-01-24 11:41:45 +0100

    Cleen Projects

--- src/Main/Projects/Projects.js
+++ src/Main/Projects/Projects.js
@@ -1,12 +1,8 @@
-import React from 'react'
-
-import TodoApp from './TodoApp/TodoApp'
-
 function Projects() {
     return (
         <>
         <h2>Projects</h2>
-        <TodoApp />
+
         </>
     )
 }
